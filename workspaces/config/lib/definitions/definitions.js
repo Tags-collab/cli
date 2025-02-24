04434335cbbd90104b2529f9aec64c7a512d0d65
Gar <gar+gh@danger.computer>
2025-02-24 10:41:01 -0800

    feat: add `node-gyp` config
    This formalizes the `node-gyp` config that is eventually consumed by [@npmcli/run-script](npm.im/@npmcli/run-script).
    
    That module will need to be updated so that it can accept this config and use it if found, only falling back to its current behavior by default.

--- workspaces/config/lib/definitions/definitions.js
+++ workspaces/config/lib/definitions/definitions.js
@@ -1294,6 +1294,16 @@ const definitions = {
     `,
     flatten,
   }),
+  'node-gyp': new Definition('node-gyp', {
+    default: require.resolve('node-gyp/bin/node-gyp.js'),
+    type: path,
+    description: `
+      This is the location of the "node-gyp" bin.  By default it uses one that ships with npm itself.
+
+      You can use this config to specify your own "node-gyp" to run.
+    `,
+    flatten,
+  }),
   'node-options': new Definition('node-options', {
     default: null,
     type: [null, String],
